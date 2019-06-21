9520e0253c4b089b810fed64983cec934188b849
mazdak <nazemi.works@gmail.com>
2019-06-21 06:48:54 -0700

    make connect app wiht child

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <!-- come from componet = we define here -->
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+// comminucate from child to parent emitting event
+// comminucate from parent to child props
+
 import SearchBar from "./components/SearchBar";
 
 export default {
   name: "App",
   components: {
     SearchBar
+  },
+  methods: {
+    //event.target.value
+    onTermChange(searchTerm) {
+      console.log(searchTerm);
+    }
   }
 };
 </script>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <input type="text">
+    <!-- directive v-on:input="onInput" -->
+    <input @input="onInput" type="text">
   </div>
 </template>
 
 
 <script>
 export default {
-  name: "SearchBar"
+  name: "SearchBar",
+  //give ability to change data
+  methods: {
+    //name of event event + verb
+    onInput(e) {
+      this.$emit("termChange", e.target.value);
+    }
+  }
 };
 </script>
 
