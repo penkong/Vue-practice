09e4d26f2406e22f2800b0ce70fa86e47dc1a4d5
mazdak <nazemi.works@gmail.com>
2019-06-21 09:01:35 -0700

    make request from vue

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,10 @@
 <script>
 // comminucate from child to parent emitting event
 // comminucate from parent to child props
-
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+
+const API_KEY = "AIzaSyCZJ14iQvhn3A6lKR5VM9mO18gw5p5XdlM";
 
 export default {
   name: "App",
@@ -19,7 +21,16 @@ export default {
   methods: {
     //event.target.value
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet", //what type of info want back
+            q: searchTerm
+          }
+        })
+        .then(res => console.log(res.data));
     }
   }
 };
