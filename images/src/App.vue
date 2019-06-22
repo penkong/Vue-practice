86f15c73a74a267ef41f7998e550022400e34a71
mazdak <nazemi.works@gmail.com>
2019-06-22 01:57:03 -0700

    starter with header

--- images/src/App.vue
+++ images/src/App.vue
@@ -1,56 +1,18 @@
 <template>
   <div class="container">
-    <!-- come from componet = we define here -->
-    <SearchBar @termChange="onTermChange"></SearchBar>
-    <!-- name we pass v-bind:= name of data in parent -->
-    <div class="row">
-      <VideoDetail :video="selectedVideo"></VideoDetail>
-      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
-    </div>
-    <!-- {{ videos.length }} -->
+    <AppHeader></AppHeader>
   </div>
 </template>
 
 <script>
 // comminucate from child to parent emitting event
 // comminucate from parent to child props
-import axios from "axios";
-import SearchBar from "./components/SearchBar";
-import VideoList from "./components/VideoList";
-import VideoDetail from "./components/VideoDetail";
-
-const API_KEY = "AIzaSyCZJ14iQvhn3A6lKR5VM9mO18gw5p5XdlM";
-
+// vuex is same as redux
+import AppHeader from "./components/AppHeader";
 export default {
   name: "App",
   components: {
-    SearchBar,
-    VideoList,
-    VideoDetail
-  },
-  data() {
-    //update this props cause app to rerender
-    return { videos: [], selectedVideo: null };
-  },
-  methods: {
-    //event.target.value
-    onTermChange(searchTerm) {
-      axios
-        .get("https://www.googleapis.com/youtube/v3/search", {
-          params: {
-            key: API_KEY,
-            type: "video",
-            part: "snippet", //what type of info want back
-            q: searchTerm
-          }
-        })
-        .then(res => {
-          this.videos = res.data.items;
-        });
-    },
-    onVideoSelect(video) {
-      this.selectedVideo = video;
-    }
+    AppHeader
   }
 };
 </script>
---
+++ images/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Image Storage
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
+
