e57e8cd7ba380a78ba7daf01dd309d6aeae4ba60
mazdak <nazemi.works@gmail.com>
2019-06-23 02:25:43 -0700

    make login work

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="ui secondary pointing menu">
     <a class="active item" href="/">Image Storage</a>
+    <div class="right menu">
+      <a class="ui item" @click="login" href="#">Login</a>
+    </div>
   </div>
 </template>
 
 <script>
+//action change data getters retrieve data
+import { mapActions } from "vuex";
+
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  methods: {
+    ...mapActions(["login"])
+  }
 };
 </script>
 
